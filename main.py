73ba9b8c339f9340cb24b104f41a263501dae345
HenryLi-0 <bxsci8266@gmail.com>
2024-11-04 23:33:40 -0500

    fix new placeover cropping and update render.py

--- main.py
+++ main.py
@@ -6,11 +6,3 @@ from subsystems.window import Window
 
 window = Window()
 window.start()
-
-
-'''
-original:
-avg of 1095 frames: 0.03700961282808487 seconds
-new:
-avg of 1095 frames: 0.0294141542965963 seconds
-'''
